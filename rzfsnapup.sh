2677c61e685bea31626ae5be1efdda145d4b41fd
Alexander Dahl <post@lespocky.de>
2014-04-28 09:59:25 +0200

    fix whitespace and -h handling. implement sparse trees

--- rzfsnapup.sh
+++ rzfsnapup.sh
@@ -12,11 +12,11 @@ ZFSNAP='/usr/sbin/zfSnap'
 
 # function definitions
 print_usage() {
-        echo "Usage: $0 remotehost remotepath localzfs"
-        echo ''
-        echo 'Options:'
-        echo '  -h             this help'
-        echo '  -s sparseconf  config file for sparse trees'
+	echo "Usage: $0 remotehost remotepath localzfs"
+	echo ''
+	echo 'Options:'
+	echo '  -h             this help'
+	echo '  -s sparseconf  config file for sparse trees'
 }
 
 # "main"
@@ -25,35 +25,56 @@ print_usage() {
 ARGS=`getopt hs: $*`
 if [ $? -ne 0 ]
 then
-        print_usage
-        exit 2
+	print_usage
+	exit 2
 fi
 set -- $ARGS
 
 while true
 do
-        case "$1" in
-                -h)
-                        print_usage
-                        shift
-                        ;;
-                -s)
-                        SPARSE_CONFIG="$2"
-                        shift; shift
-                        ;;
-                --)
-                        shift; break;;
-        esac
+	case "$1" in
+		-h)
+			print_usage
+			exit 0
+			;;
+		-s)
+			SPARSE_CONFIG="$2"
+			shift; shift
+			;;
+		--)
+			shift; break;;
+	esac
 done
 
 # sync with rsync
 if [ -z "${SPARSE_CONFIG}" ]
 then
-    echo "> syncing ${1}:${2}"
-    ${RSYNC} --stats -zaH --delete -e "${SSH} -i /root/.ssh/backup" ${1}:${2}/ /${3}
-    echo ''
+	echo "> syncing ${1}:${2}"
+	${RSYNC} --stats -zaH --delete -e "${SSH} -i /root/.ssh/backup" ${1}:${2}/ /${3}
+	echo ''
 else
-    echo "using ${SPARSE_CONFIG} as sparse tree config"
+	if [ -f "${SPARSE_CONFIG}" ]
+	then
+		echo "using ${SPARSE_CONFIG} as sparse tree config"
+		. ${SPARSE_CONFIG}
+	else
+		echo "sparse config '${SPARSE_CONFIG}' not found"
+		exit 1
+	fi
+
+	for _idx in $(seq ${SPARSE_FOLDER_N})
+	do
+		eval _folder='$SPARSE_FOLDER_'${_idx}
+		if [ -z "${_folder}" ]
+		then
+			break
+		fi
+
+		mkdir -p /${3}/${_folder}
+		echo "> syncing ${1}:${2}/${_folder}"
+		${RSYNC} -zaH --delete -e "${SSH} -i /root/.ssh/backup" \
+			${1}:${2}/${_folder}/ /${3}/${_folder}
+	done
 fi
 
 # take snapshot
@@ -62,14 +83,14 @@ DAY_OF_WEEK="$(${DATE} +%u)"
 
 if [ "${DAY_OF_MONTH}" = '12' ]
 then
-        echo '> creating monthly zfs snapshot'
-        ${ZFSNAP} -v -s -S -a 2y ${3}
+	echo '> creating monthly zfs snapshot'
+	${ZFSNAP} -v -s -S -a 2y ${3}
 elif [ "${DAY_OF_WEEK}" = '3' ]
 then
-        echo '> creating weekly zfs snapshot'
-        ${ZFSNAP} -v -s -S -a 2m ${3}
+	echo '> creating weekly zfs snapshot'
+	${ZFSNAP} -v -s -S -a 2m ${3}
 else
-        echo '> creating daily zfs snapshot'
-        ${ZFSNAP} -v -s -S -a 2w ${3}
+	echo '> creating daily zfs snapshot'
+	${ZFSNAP} -v -s -S -a 2w ${3}
 fi
 echo ''
